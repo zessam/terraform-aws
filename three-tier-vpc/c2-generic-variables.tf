0c9000467b43367ac1630316b970a8cab2c8c5d3
Zeyad Essam <72177003+zessam@users.noreply.github.com>
2024-05-26 19:52:06 +0300

    three-tier-vpc

---
+++ three-tier-vpc/c2-generic-variables.tf
@@ -0,0 +1,23 @@
+# Input Variables
+
+# AWS Region
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type        = string
+  default     = "eu-west-1"
+}
+
+# Environment Variable
+variable "environment" {
+  description = "Environment Vriable used as a prefix"
+  type        = string
+  default     = "dev"
+}
+
+
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type        = string
+  default     = "DevOps"
+}
---
+++ three-tier-vpc/terraform.tfvars
@@ -0,0 +1,4 @@
+# Generic Variables
+aws_region = "eu-west-1"
+environment = "dev"
+business_divsion = "DevOps"
